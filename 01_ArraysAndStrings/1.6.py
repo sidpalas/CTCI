f15b1a4209a88978d5c0ed514e1f0967d2927b3c
Sid Palas <sid.palas@gmail.com>
2017-12-03 17:23:09 -0800

    Fixed slow implementation for string concatenation from += to "".join()

--- 01_ArraysAndStrings/1.6.py
+++ 01_ArraysAndStrings/1.6.py
@@ -2,7 +2,11 @@ def strCompressor(inputStr):
     currentRepeat = 1
     maxRepeat = 1
     prevChar = inputStr[0]
-    outputStr = prevChar
+    #on my first pass I used += to build up the strings.  This is apparently very slow
+    #because strings are immutable in python so it had to copy the full string each time
+    #leading to O(N^2) complexity.  Building an array and then joining at the end makes
+    #this O(N) 
+    outputStrList = [prevChar]
     for char in inputStr[1:]:
         if char == prevChar:
             currentRepeat += 1
@@ -13,15 +17,15 @@ def strCompressor(inputStr):
             #the check at the bottom would be to compare len(outputStr) to len(inputStr) or you could keep
             #track of how many characters each substitution was saving/harming throughout
             if currentRepeat > 1:
-                outputStr += str(currentRepeat)
-            outputStr += char
+                outputStrList.append(str(currentRepeat))
+            outputStrList.append(char)
             prevChar = char
             currentRepeat = 1
     if currentRepeat > 1: #handle the final character sequence
-        outputStr += str(currentRepeat)
+        outputStrList.append(str(currentRepeat))
 
     if maxRepeat > 2:
-        return outputStr
+        return "".join(outputStrList)
     else:
         return inputStr
 
