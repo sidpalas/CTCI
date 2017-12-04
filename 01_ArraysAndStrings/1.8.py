6f8e87418404b6c917f89a5a752f797075c64487
Sid Palas <sid.palas@gmail.com>
2017-12-04 00:22:23 -0800

    Improved storage efficiency by only needing N+M elements instead of M*N to store null map

--- 01_ArraysAndStrings/1.8.py
+++ 01_ArraysAndStrings/1.8.py
@@ -1,17 +1,19 @@
 def matrixZeroer(matrix):
     M = len(matrix)
     N = len(matrix[0])
-    zeroMap = [[1]*N for i in range(M)]
+    #didnt need to be storing the full matrix... instead just the row and columns
+    #need to be zeroed... this saves storage space (doesnt speed up algorithm though)
+    rowNulls = [False]*M
+    colNulls = [False]*N
     for i in range(M):
         for j in range(N):
             if matrix[i][j] == 0:
-                for k in range(M):
-                    zeroMap[k][j] = 0
-                for l in range(N):
-                    zeroMap[i][l] = 0
+                rowNulls[i] = True
+                colNulls[j] = True
     for i in range(M):
         for j in range(N):
-            matrix[i][j] = matrix[i][j] * zeroMap[i][j]
+            if rowNulls[i] or colNulls[j]:
+                matrix[i][j] = 0
     return matrix
 
 print matrixZeroer([[1,2,3],[4,5,6],[7,8,9],[10,11,12]])
