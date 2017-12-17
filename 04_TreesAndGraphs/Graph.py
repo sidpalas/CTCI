c7bbd2261caa5b5edeab7c5d33e1dc7771965591
Sid Palas <sid.palas@gmail.com>
2017-12-16 17:29:53 -0800

    Added Graph class to use in other problems

---
+++ 04_TreesAndGraphs/Graph.py
@@ -0,0 +1,8 @@
+class Graph(object):
+    def __init__(self):
+        self.nodes = []
+
+class Node(object):
+    def __init__(self, name, children):
+        self.name = name
+        self.children = children
