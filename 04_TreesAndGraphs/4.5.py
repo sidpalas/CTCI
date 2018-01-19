e984e11928b5d5c47eaca4ab89a98044cb121858
Sid Palas <sid.palas@gmail.com>
2018-01-18 21:44:51 -0800

    Adjusted to return True/False rather than simply printing when a violation occurs

--- 04_TreesAndGraphs/4.5.py
+++ 04_TreesAndGraphs/4.5.py
@@ -3,21 +3,26 @@ from Tree import minTree
 import sys
 
 def checkBST(root, minBound, maxBound):
-    print("%s <= %s < %s ?" % (minBound, root.getData(), maxBound))
-    if root.getData() == None:
-        return
-    if root.getData() >= minBound and root.getData() < maxBound:
-        if root.getLeft():
-            checkBST(root.getLeft(), minBound, root.getData())
-        if root.getRight():
-            checkBST(root.getRight(), root.getData(), maxBound)
+    #print("%s <= %s < %s ?" % (minBound, root.getData(), maxBound))
+    if root == None:
+        return True
+    if not (root.getData() >= minBound and root.getData() < maxBound):
+        return False
     else:
-        print "NOT A BST!!!"
+        return (checkBST(root.getLeft(), minBound, root.getData()) and checkBST(root.getRight(), root.getData(), maxBound))
+
+def isValidBST(root):
+    if root.getData() == None:
+        return True
+
+    minBound = -sys.maxint-1
+    maxBound = sys.maxint
+    return (checkBST(root.getLeft(), minBound, root.getData()) and checkBST(root.getRight(), root.getData(), maxBound))
 
 testTree1 = BinaryNode(8,BinaryNode(4,BinaryNode(2),BinaryNode(6)),BinaryNode(10,None,BinaryNode(20)))
-checkBST(testTree1, -sys.maxint-1, sys.maxint)
+print isValidBST(testTree1)
 print "test1 complete"
 
 testTree2 = BinaryNode(8,BinaryNode(4,BinaryNode(2),BinaryNode(12)),BinaryNode(10,None,BinaryNode(20)))
-checkBST(testTree2, -sys.maxint-1, sys.maxint)
+print isValidBST(testTree2)
 print "test2 complete"
