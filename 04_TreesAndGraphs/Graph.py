d761d6ac377d9ec3fc32e3367677ec0e81720f20
Sid Palas <sid.palas@gmail.com>
2018-02-06 15:56:45 -0800

    Added solution to 4.7

--- 04_TreesAndGraphs/Graph.py
+++ 04_TreesAndGraphs/Graph.py
@@ -2,7 +2,17 @@ class Graph(object):
     def __init__(self):
         self.nodes = []
 
+    def addNode(self, node):
+        self.nodes.append(node)
+
 class Node(object):
-    def __init__(self, name, children = []):
+    def __init__(self, name, children = [], parents = []):
         self.name = name
         self.children = children
+        self.parents = parents
+
+    def addChild(self, child):
+        self.children.append(child)
+
+    def addParent(self, parent):
+        self.parents.append(parent)
