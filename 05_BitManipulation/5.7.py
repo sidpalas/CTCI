a149e3fcab00e1f85bc0ae220589e1acb8de30e1
Sid Palas <sid.palas@gmail.com>
2018-02-09 15:37:25 -0800

    Added solution to 5.7

---
+++ 05_BitManipulation/5.7.py
@@ -0,0 +1,7 @@
+def pairwiseSwap(num):
+    #Assumes max of 32 bit values
+    maskOdd = int('01'*16,2)
+    maskEven = int('10'*16,2)
+    return ((num & maskOdd) << 1) | ((num & maskEven) >> 1)
+
+print(bin(pairwiseSwap(int('1110110101',2)))) #1101111010
