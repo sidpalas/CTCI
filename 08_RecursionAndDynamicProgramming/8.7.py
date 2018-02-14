0db307efaa19e69b39058236a23f3ac61e8561c1
Sid Palas <sid.palas@gmail.com>
2018-02-14 10:43:13 -0800

    improved 8.7 to work natively with strings instead of lists of chars

--- 08_RecursionAndDynamicProgramming/8.7.py
+++ 08_RecursionAndDynamicProgramming/8.7.py
@@ -1,12 +1,19 @@
 def permuteNoDups(stringIn):
     if len(stringIn) == 1:
-        return [[stringIn]]
+        return [stringIn]
     else:
         permutes = []
         prevPermutes = permuteNoDups(stringIn[0:-1])
         for permutation in prevPermutes:
-            for i in range(0,len(permutation + 1)):
-                permutes.append(permutation.insert(stringIn[-1],i))
+            for i in range(0,len(permutation) + 1):
+                temp = permutation[0:i] + stringIn[-1] + permutation[i:]
+                permutes.append(temp)
         return permutes
 
 print(permuteNoDups('a'))
+
+print(permuteNoDups('ab'))
+
+print(permuteNoDups('abc'))
+
+print(permuteNoDups('abcd'))
