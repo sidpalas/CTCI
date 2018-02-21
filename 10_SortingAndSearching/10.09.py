b0fc9f798585ef21eba6b42cedffaa5718964161
Sid Palas <sid.palas@gmail.com>
2018-02-21 12:56:20 -0800

    Fixed algorithm for 10.9

--- 10_SortingAndSearching/10.09.py
+++ 10_SortingAndSearching/10.09.py
@@ -1,4 +1,8 @@
-def findDiagonalPair(gridIn, searchVal):
+def gridBinarySearch(gridIn, searchVal, combinedRowOffset = 0, combinedColOffset = 0):
+    if not gridIn:
+        return None
+    if not gridIn[0]:
+        return None
     numRows = len(gridIn)
     numCols = len(gridIn[0])
     rowOffset = 0
@@ -20,72 +24,46 @@ def findDiagonalPair(gridIn, searchVal):
             high = mid - 1
         else: #compareVal < searchVal:
             low = mid + 1
-        mid = (low + high) // 2
+        mid = max(((low + high) // 2),0)
 
-    return mid + rowOffset, mid + colOffset
-
-def binarySearch(arrayIn, searchVal):
-    if not arrayIn:
-        return -1
-    low = 0
-    high = len(arrayIn) - 1
-    mid = (low + high) // 2
-    while low < high:
-        if arrayIn[mid] == searchVal:
-            return mid
-        elif arrayIn[mid] > searchVal:
-            high = mid - 1
-        else:
-            low = mid + 1
-        mid = (low + high) // 2
-
-    if arrayIn[mid] == searchVal:
-        return mid
-    else:
-        return -1
-
-def searchGrid(gridIn, searchVal):
-    (diagRow, diagCol) = findDiagonalPair(gridIn, searchVal)
-    compareVal = gridIn[diagRow][diagCol]
-
-    if compareVal < searchVal: #ensure you are starting above searchVal
-        if diagRow == len(gridIn) - 1 or diagCol == len(gridIn[0]) - 1:
-            return -1, -1 #already as high as you can go
-        diagRow += 1
-        diagCol += 1
-        compareVal = gridIn[diagRow][diagCol]
-
+    compareVal = gridIn[mid + rowOffset][mid + colOffset]
     if compareVal == searchVal:
-        return diagRow, diagCol
-    elif compareVal > searchVal:
-        #search this column
-        colSearch = binarySearch(gridIn[diagRow][0:diagCol], searchVal)
-        if colSearch >= 0:
-            return diagRow, colSearch
-        #search this row
-        rowSearch = binarySearch([row[diagCol] for row in gridIn[0:diagRow]], searchVal)
-        # print([row[diagCol] for row in gridIn[0:diagRow]], rowSearch)
-        if rowSearch >= 0:
-            return rowSearch, diagCol
+        return mid + rowOffset + combinedRowOffset, mid + colOffset + combinedColOffset
+    if compareVal < searchVal:
+        if (mid + rowOffset) == len(gridIn) - 1 or (mid + colOffset) == len(gridIn[0]) - 1:
+            return None #already as high as you can go
+        rowOffset += 1
+        colOffset += 1
 
-    return -1, -1 #searchVal not found...
+    compareVal = gridIn[mid + rowOffset][mid + colOffset]
 
+    topRightGrid = [row[(mid+ colOffset):] for row in gridIn[:(mid + rowOffset)]]
+    bottomLeftGrid = [row[:(mid+ colOffset)] for row in gridIn[(mid + rowOffset):]]
+
+    return gridBinarySearch(topRightGrid, searchVal, combinedRowOffset, combinedColOffset + (mid + colOffset)) \
+            or gridBinarySearch(bottomLeftGrid, searchVal,  combinedRowOffset + (mid + rowOffset), combinedColOffset)
 
 testGrid = [list(range(1,7)),\
             list(range(2,8)),\
             list(range(3,9)),\
             list(range(4,10))]
 
+testGrid[0][0] = 0
+
 testGrid[1][5] = 8
 testGrid[2][5] = 9
 testGrid[3][5] = 10
 
+
+
 #by not drawing a good test grid initially I fooled myself into thinking element
 #[1][5] can't be greater than [2][4]... this is not true. Instead should have
-#recursively searched the top right and bottom right subgrids recursively 
+#recursively searched the top right and bottom right subgrids recursively
+
+#fixed...
 
 for row in testGrid:
     print(row)
 
-for i in range(0,11):
-    print(searchGrid(testGrid, i))
+for i in range(0,12):
+    print(i, gridBinarySearch(testGrid, i))
